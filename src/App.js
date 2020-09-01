c216d3ab4cbb4419b2d538e1ade295248580be1e
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-09-01 12:23:48 +0600

    Creating Navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import Home from "./pages/Home";
-import Room from "./pages/Room";
+import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import {Route,Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <Fragment>
-      <Home/>
-      <Room/>
-      <SingleRoom/>
-      <Error/>
+        <Navbar/>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/rooms" component={Rooms}/>
+            <Route exact path="/rooms/:slug" component={SingleRoom}/>
+            <Route component={Error}/>
+        </Switch>
+
     </Fragment>
   );
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Room = () => {
+const Rooms = () => {
     return (
         <div>
             Hello from rooms page
@@ -8,4 +8,4 @@ const Room = () => {
     );
 };
 
-export default Room;
+export default Rooms;
