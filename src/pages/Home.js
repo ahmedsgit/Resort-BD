ee0328e7a5bde7be39443975989d0718ab88105b
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-09-03 13:30:50 +0600

    created Single Room  component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,19 @@ import Banner from "../components/Banner";
 import {Link} from "react-router-dom";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
+import Button from '../components/StyledHero';
 
 function Home() {
     return (
         <Fragment>
-        <Hero>
-            <Banner title="Luxurious Rooms" subtitle="deluxe rooms startting at $299">
-                <Link to='/rooms' className='btn-primary'>our rooms</Link>
-            </Banner>
-        </Hero>
-        <Services/>
-        <FeaturedRooms/>
+            <Hero>
+                <Banner title="Luxurious Rooms" subtitle="deluxe rooms startting at $299">
+                    <Link to='/rooms' className='btn-primary'>our rooms</Link>
+                </Banner>
+            </Hero>
+            <Services/>
+            <FeaturedRooms/>
+            <Button>Hi</Button>
         </Fragment>
     );
 }
